ba8a114661b9cdeece47aa025d61951531a62e57
DavidHermi <91197896+DavidHermi@users.noreply.github.com>
2021-11-30 10:41:08 +0100

    Add files via upload
    Boletin12

---
+++ Boletin12/src/boletin12/Garaxe.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin12;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author dam1
+ */
+public class Garaxe {
+    int numeroCoches;
+        String matricula;
+
+        public void setNumeroCoches() {
+            numeroCoches = Integer.parseInt(JOptionPane.showInputDialog("Numero de coches"));
+        }
+
+        public void setMatricula() {
+            matricula = JOptionPane.showInputDialog("Numero de coches");
+        }
+
+        public int getNumeroCoches() {
+            return numeroCoches;
+        }
+
+        public String getMatricula() {
+            return matricula;
+        }
+
+        public void sitio() {
+
+            if (numeroCoches < 5) {
+                JOptionPane.showMessageDialog(null, "PLAZAS DISOPNIBLES");
+            } else {
+                JOptionPane.showMessageDialog(null, "COMPLETO");
+            }
+        }
+
+        public void factura() {
+
+            float precio = 0;
+
+            int tiempo = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantas horas estará aparcado el coche?"));
+
+            if (tiempo < 3) {
+                precio = tiempo * 0.50f;
+            }
+            if (tiempo >= 3) {
+                tiempo = tiempo - 3;
+                precio = 1.5f + tiempo * 0.20f;
+            }
+
+
+
+
+            int cartosRecibidos = Integer.parseInt(JOptionPane.showInputDialog("El coste del garaje es de "+precio+"€\nPago"));
+            float cartosDevoltos =  cartosRecibidos - precio ;
+
+
+            JOptionPane.showMessageDialog(null, "FACTURA\nMATRICULA DO COCHE: "+ matricula+"\nTEMPO: "+tiempo+"\nPRECIO: "+precio+"\nCARTOS RECIBIDOS: "+cartosRecibidos+"\nCARTOS DEOVLTOS: "+cartosDevoltos   );
+
+
+        }
+
+    }
+
